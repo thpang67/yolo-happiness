49895b65cfd541cb46225305f90e133a98535d61
Thomas S. Pangborn <thpang67@users.noreply.github.com>
2015-03-08 19:26:19 -0400

    Update email-test.rb
    Adding content to server file.

--- email-test.rb
+++ email-test.rb
@@ -3,3 +3,34 @@
 # back the command executed along with a few other bits. It will
 # parse the JSON item returned.
 #
+
+# required modules
+require 'sinatra'
+require 'json'
+require 'net/smtp'
+
+# 
+def sendEmail(from, to, repo, action, result)
+    
+    # variables
+    @repo   = repo
+    @action = action
+    @result = result
+    
+    message << MESSAGE_END
+From: #{from}
+To: #{to}
+Subject: #{repo} action: #{action}
+
+This email is to inform you of the following:
+
+Repository : #{repo}
+Action     : #{action}
+Result     : #{result}
+MESSAGE_END
+
+end
+
+post '/github/payload' do
+    push = JSON.parse(request.body.head)
+end
